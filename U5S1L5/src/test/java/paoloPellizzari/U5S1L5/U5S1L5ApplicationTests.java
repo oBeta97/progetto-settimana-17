7764c03455ac23a80f4c562cbb84d074bd454538
oBeta97 <paolopellizzari97@gmail.com>
2024-10-25 10:51:03 +0200

    creazione progetto e immagine schema ER

---
+++ U5S1L5/src/test/java/paoloPellizzari/U5S1L5/U5S1L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package paoloPellizzari.U5S1L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S1L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
